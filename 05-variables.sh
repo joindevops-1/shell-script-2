df2beaca83803dd977ccfc721c0e491a6ceed996
joindevops <info@joindevops.com>
2024-04-15 10:44:57 +0530

    profile

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,9 +2,9 @@
 
 echo "Please enter your username:: "
 
-read USERNAME #the value entered above will be automatically atteched to USERNAME variable
+read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
 
 echo "Please enter your Password:: "
-read PASSWORD
+read -s PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD" # I am priting just for validation, you should not print username and passwords in scripts
