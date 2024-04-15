e29fc965285aabd83cf1d59c0e8e141e5b1157c0
joindevops <info@joindevops.com>
2024-04-15 12:31:46 +0530

    profile

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -4,5 +4,6 @@ echo "All variables: $@"
 echo "Number of variables passed: $#"
 echo "Script name: $0"
 echo "Process ID of the current shell: $$"
+sleep 2000 &
 echo "Process ID of the last background command: $!"
 echo "Process ID of the current shell: $"
