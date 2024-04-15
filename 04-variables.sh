7f845cc02e1fb48441e2bd3e73d7c1bbf33f4027
joindevops <info@joindevops.com>
2024-04-15 10:42:00 +0530

    profile

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: work is boring. I want to upgrade to DevOps"
