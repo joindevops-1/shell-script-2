f9b596e22a46d4c0d8b23833e2000b3c1253266c
joindevops <info@joindevops.com>
2024-04-15 12:33:32 +0530

    profile

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -7,3 +7,7 @@ echo "Process ID of the current shell: $$"
 sleep 2000 &
 echo "Process ID of the last background command: $!"
 echo "Process ID of the current shell: $"
+echo "Current working directory: $PWD"
+echo "home directory of the user: $home"
+echo "Current user: $user"
+echo "hostname: $hostname"
