c494bbe0d81a857527d593ef6860526d0e3e418e
joindevops <info@joindevops.com>
2024-04-19 12:35:08 +0530

    functions

--- 15-other-script.sh
+++ 15-other-script.sh
@@ -3,3 +3,4 @@
 COURSE="DevOps with AWS"
 
 echo "Course from other script: $COURSE"
+echo "PID from other script: $$"
--- 16-current-script.sh
+++ 16-current-script.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 COURSE=DevOps
+echo "PID from current script: $$"
 pwd
 ls -l 15-other-script.sh
 source ./15-other-script.sh
