adb158d8278cacfd17274a9ec2d4dcccb18522af
joindevops <info@joindevops.com>
2024-04-20 09:49:38 +0530

    functions

--- 16-current-script.sh
+++ 16-current-script.sh
@@ -4,9 +4,9 @@ COURSE="DevOps"
 echo "Course before calling other script: $COURSE"
 echo "PID from current script before calling other script: $$"
 
-./15-other-script.sh
+#./15-other-script.sh
 
-#source ./15-other-script.sh
+source ./15-other-script.sh
 
 echo "Course After calling other script: $COURSE"
 echo "PID from current script After calling other script: $$"
