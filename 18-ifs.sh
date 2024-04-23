4b1da72cda4889c0c4613dbab37872495841e559
joindevops <info@joindevops.com>
2024-04-23 13:15:56 +0530

    functions

--- 18-ifs.sh
+++ 18-ifs.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-SOURCE_DIRECTORY=/tmp/app-logs
+file=/etc/passwd
+
+if [ ! -f $file ]
+then
+    echo "$file file does not exist."
+    exit 1
+fi
 
 R="\e[31m"
 G="\e[32m"
@@ -10,4 +16,4 @@ N="\e[0m"
 while IFS= read -r line
 do
     echo "$line"
-done < /etc/passwd
+done < $file
